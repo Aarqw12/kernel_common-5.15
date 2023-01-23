a36415edd5f17eb5f7cef876d3b83178731de6cb
André Draszik <draszik@google.com>
2023-01-23 13:41:11 +0000

    i2c: exynos5: add missing pinctrl headers (compile against v6.2 kernel)
    The v6.2 has tidied up the pinctrl headers in [1], and now this
    driver here fails compilation.
    
    We should really be including all headers that we need, rather than
    relying on some of the generic headers to include them for us.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e5530adc17a7
        ("pinctrl: Clean up headers")
    
    Bug: 266383745
    Change-Id: I7313bec02a31ac165079260d7108c1ef52f4dabd
    Signed-off-by: André Draszik <draszik@google.com>

--- drivers/i2c/busses/i2c-exynos5.c
+++ drivers/i2c/busses/i2c-exynos5.c
@@ -14,6 +14,7 @@
 #include <linux/delay.h>
 #include <linux/errno.h>
 #include <linux/err.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/slab.h>
